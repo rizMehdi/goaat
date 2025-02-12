619abd31520b06c58eacae4ca6ada23b829c12aa
rizMehdi <ccmeall@gmail.com>
2025-02-12 15:41:13 +0000

    sidebar

--- app.py
+++ app.py
@@ -9,31 +9,41 @@ def load_image(image_path):
         return None
 
 # Streamlit App Configuration
-st.set_page_config(page_title="Output Area Analysis", layout="centered")
+st.set_page_config(page_title="GNN Output Area Analysis Toolkit", layout="centered")
 
 # Display logos
-logo_paths = ["img/logo1.png", "img/logo2.png", "img/logo3.png"]
+logo_paths = ["img/prime.png", "img/hwu.png", "img/ukri.png"]
 st.image([img for img in logo_paths if os.path.exists(img)], width=100)
 
 # Page Title
 st.title("Output Area Analysis")
 
-# Dropdown Options
+# Sidebar for Dropdown Selections
+st.sidebar.header("Select Options")
+
 cities = ["Birmingham", "Bradford", "York", "Bristol", "London", "Manchester", "Leeds", "Liverpool", "Sheffield", "Coventry", "Leicester", "Nottingham", "Newcastle", "Southampton", "Portsmouth", "Brighton", "Plymouth", "Derby", "Stoke-on-Trent", "Wolverhampton", "Norwich", "Oxford", "Cambridge", "Bath", "Exeter", "Durham", "Lancaster", "Chester", "Hull", "Sunderland", "Ipswich", "Reading", "Milton Keynes", "Northampton", "Luton", "Swindon", "Worcester", "Gloucester", "Carlisle", "Lincoln", "Chelmsford", "Preston", "Blackpool", "Bolton", "Stockport", "Warrington", "Rochdale", "Oldham", "Bournemouth", "Poole", "Worthing", "Basildon", "Southend-on-Sea", "Middlesbrough", "Blackburn", "Burnley", "Telford", "Slough", "Wakefield", "Doncaster", "Rotherham", "Huddersfield", "Southport", "Peterborough", "Guildford", "Basingstoke", "Woking"]
 
-categories = ["Property", "Poverty", "Economic", "Health", "Eth", "Energy"]
+category_mapping = {
+    "House Ownership": "Property",
+    "Poverty": "Poverty",
+    "Economic": "Economic",
+    "Health": "Health",
+    "Ethnicity": "Eth",
+    "Energy": "Energy"
+}
 
+categories = list(category_mapping.keys())
 classifications = [f"Class_{i}" for i in range(8)]
 
-# Dropdown Selections
-city = st.selectbox("Select a City", cities)
-category = st.selectbox("Select a Category", categories)
-classification = st.selectbox("Select a Classification", classifications)
+city = st.sidebar.selectbox("Select a City", cities)
+category = st.sidebar.selectbox("Select a Category", categories)
+classification = st.sidebar.selectbox("Select a Classification", classifications)
 
 # Show Images Button
-if st.button("Show Images"):
+if st.sidebar.button("Show Images"):
+    category_path = category_mapping[category]
     left_image_path = f"img/{city}.png"
-    right_image_path = f"img/{city}_{category}_{classification}.png"
+    right_image_path = f"img/{city}_{category_path}_{classification}.png"
     
     left_image = load_image(left_image_path)
     right_image = load_image(right_image_path)
@@ -44,10 +54,10 @@ if st.button("Show Images"):
         if left_image:
             st.image(left_image, caption=f"{city} Image")
         else:
-            st.error("Left image not found")
+            st.error("Data for this city is yet to be integrated.")
     
     with col2:
         if right_image:
             st.image(right_image, caption=f"{city} {category} {classification} Image")
         else:
-            st.error("Right image not found")
+            st.error("Data dor this category/class is yet to be integrated.")
