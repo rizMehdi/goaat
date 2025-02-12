73710b7cfd1c9476322b98f3ef9628293816088a
rizMehdi <ccmeall@gmail.com>
2025-02-12 16:46:57 +0000

    one by one dropdown

--- app.py
+++ app.py
@@ -47,11 +47,19 @@ categories = list(category_mapping.keys())
 classifications = [f"Class_{i}" for i in range(8)]
 
 city = st.sidebar.selectbox("Select a Location", [""] + cities)
-category = st.sidebar.selectbox("Select a Category", categories)
-classification = st.sidebar.selectbox("Select a Classification", classifications)
+
+if city:
+    category = st.sidebar.selectbox("Select a Category", [""] + categories)
+else:
+    category = None
+
+if category:
+    classification = st.sidebar.selectbox("Select a Classification", classifications)
+else:
+    classification = None
 
 # Show Images Button
-if st.sidebar.button("Show Results"):
+if city and category and classification and st.sidebar.button("Show Results"):
     category_path = category_mapping[category]
     left_image_path = f"img/{city}.png"
     right_image_path = f"img/{city}_{category_path}_{classification}.png"
@@ -59,27 +67,6 @@ if st.sidebar.button("Show Results"):
     left_image = load_image(left_image_path)
     right_image = load_image(right_image_path)
     
-    # col1, col2 = st.columns(2)
-    
-#     with col1:
-#         if left_image:
-#             st.image(left_image, caption=f"{city} Image")
-#         else:
-#             st.error("Data for this city is yet to be integrated.")
-    
-#     with col2:
-#         if right_image:
-#             st.image(right_image, caption=f"{city} {category} {classification} Image")
-#         else:
-#             st.error("Data for this category/class is yet to be integrated.")
-
-# # Display logos at the bottom of the sidebar
-# logo_paths = ["img/prime.png", "img/hwu.png", "img/ukri.png"]
-# st.sidebar.image([img for img in logo_paths if os.path.exists(img)], width=100)
-
-
-
-
     col1, col2 = st.columns(2)
         
     with col1:
