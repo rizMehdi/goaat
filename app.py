2e241ca36f5fd1e62cdb9ce5c81b2916c6c029e2
rizMehdi <ccmeall@gmail.com>
2025-03-12 23:10:32 +0000

    #fcf7fb tab

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ st.markdown("""
 	.stTabs [data-baseweb="tab"] {
 		height: 30px;
         white-space: pre-wrap;
-		background-color: #FFFFFF;
+		background-color: ##fcf7fb;
 		border-radius: 4px 4px 0px 0px;
 		gap: 10px;
 		padding-top: 10px;
