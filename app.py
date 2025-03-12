a1b7abb5db11f9f47bc6f63089d194d80d1391e2
rizMehdi <ccmeall@gmail.com>
2025-03-12 20:40:38 +0000

    add path for debug

--- app.py
+++ app.py
@@ -108,6 +108,7 @@ if city and category and st.sidebar.button("Show Results"):
     category_path = category_mapping[category]
     left_image_path = f"img/{city}.png"
     right_image_path = f"img/{city}_{category_path}_{classification}.png"
+    st.write(left_image_path, right_image_path)
     
     left_image = load_image(left_image_path)
     right_image = load_image(right_image_path)
